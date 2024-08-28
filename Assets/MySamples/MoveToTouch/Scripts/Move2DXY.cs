9a80ad13c864727f0e74b99d37e1ce2ba3f620e4
zhulei <zhulei@xmfunny.com>
2024-08-28 19:27:55 +0800

    <feature> a tank mode

--- Assets/MySamples/MoveToTouch/Scripts/Move2DXY.cs
+++ Assets/MySamples/MoveToTouch/Scripts/Move2DXY.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class Move2DXY : MonoBehaviour
 {
+    [SerializeField]
+    float speed = 5f;
+    [SerializeField]
+    float rotationSpeed = 720f;
 
     void Update()
     {
@@ -13,7 +17,20 @@ public class Move2DXY : MonoBehaviour
 
     void Move()
     {
-        transform.position = MoveToTouchManager.WorldPositionTarget;
+        if (Vector3.Distance(transform.position, MoveToTouchManager.WorldPositionTarget) > 0.1f)
+        {
+            Vector3 directionToTarget = (MoveToTouchManager.WorldPositionTarget - transform.position).normalized;
+
+            Quaternion targetRotation = Quaternion.LookRotation(directionToTarget, Vector3.back);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            if (Quaternion.Angle(transform.rotation, targetRotation) < 1f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, MoveToTouchManager.WorldPositionTarget, speed * Time.deltaTime);
+            }
+
+        }
     }
 
 }
